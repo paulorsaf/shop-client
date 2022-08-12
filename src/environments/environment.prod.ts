326fd6311991b410a4af5acab46a263752a71831
Paulo Alves <paulorsaf@gmail.com>
2022-08-12 20:44:50 +0400

    Updating API url in environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,7 @@ import { environmentDefault } from "./environment.default";
 export const environment = {
   ...environmentDefault,
 
+  api: "https://southamerica-east1-shop-354211.cloudfunctions.net/shop-client-server",
   apiCms: "https://paulorsaf.dev/shop-cms/api",
   imageBaseUrl: "https://paulorsaf.dev",
   production: true
--- src/environments/environment.stiggo.ts
+++ src/environments/environment.stiggo.ts
@@ -3,6 +3,7 @@ import { environmentDefault } from "./environment.default";
 export const environment = {
   ...environmentDefault,
 
+  api: "https://southamerica-east1-shop-354211.cloudfunctions.net/shop-client-server",
   apiCms: "https://paulorsaf.dev/shop-cms/api",
   companyId: "vmE7GfEMUAzXca85A4ek",
   imageBaseUrl: "https://paulorsaf.dev",
