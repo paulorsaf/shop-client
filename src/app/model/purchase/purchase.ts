5815854700734cb34b44ff55dbb486a6f12cc838
Paulo Alves <paulorsaf@gmail.com>
2022-08-19 17:23:49 +0400

    Fixing build errors

--- src/app/model/purchase/purchase.ts
+++ src/app/model/purchase/purchase.ts
@@ -1,6 +1,12 @@
+import { Address } from "../address/address"
+
 export type Purchase = {
     id: string;
+    
+    address: Address;
+    createdAt: string;
     payment: Payment;
+    status: string;
 }
 
 export type Payment = {
