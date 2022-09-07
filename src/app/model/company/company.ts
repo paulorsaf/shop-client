25c4025c3f73b9ee5ff4316e4e20d462d10e1485
Paulo Alves <paulorsaf@gmail.com>
2022-09-07 12:59:46 +0200

    Fixing build error

--- src/app/model/company/company.ts
+++ src/app/model/company/company.ts
@@ -3,5 +3,10 @@ import { Address } from "../address/address";
 export type Company = {
     address: Address;
     id: string;
+    logo: Image;
     pixKey: string;
+}
+
+export type Image = {
+    imageUrl: string;
 }
